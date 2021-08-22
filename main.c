5f221f0029d18ed361d6d8cb3c10185f09a5ea49
Francisco Cicerchia <francicerchia@gmail.com>
2021-08-22 17:16:06 -0300

    Segundo Push

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "scanner.h"
+
+int main()
+{
+    int getchar(void);
+    int z;
+    
+
+
+    return 0;
+}
---
+++ scanner.h
@@ -0,0 +1,9 @@
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+
+enum tokens {FDT = EOF, SEP = ',', CAD};
+
+enum tokens get_token();
+
+
+#endif
